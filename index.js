824b3237be6c14d6ac62951489a88f53f4072f7c
magnoneves <138737161+magnoneves@users.noreply.github.com>
2024-08-22 09:21:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ import express from 'express';
 import mysql from 'mysql2/promise';
 const app = express();
 const port = process.env.PORT || 3000;
+try{
+ const mysqli = mysql.createPool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        });
+}
+catch(err){
+  res.send(err);
+}
+
+
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+   const [results] = mysqli.execute('INSERT INTO usuario(nome, email, senha) VALUES (matheus, matheus, 123)');
+    console.log("Inserção feita com sucesso");
+    res.send("ola");
+ 
 });
-
 app.listen(port, () => {
   console.log(`O servidor está rodando na porta ${port}`);
 });
