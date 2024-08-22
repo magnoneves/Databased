5fb4cdcd5819a12e7e5fb3acd0e0cf02739c8298
magnoneves <138737161+magnoneves@users.noreply.github.com>
2024-08-22 08:53:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import mysql from 'mysql2/promise';
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000; // Definindo uma porta padrão se a variável de ambiente não estiver definida
+
 app.use(express.urlencoded({ extended: true }));
 
 async function init() {
+    // Rota para cadastro
+    app.post('/cadastro', async (req, res) => {
+        try {
+            res.send("Olá");
+        } catch (err) {
+            console.log("Deu erro ao fazer a inserção", err);
+            res.status(500).send("Erro ao inserir dados.");
+        }
+    });
 
-        // Rota para cadastro
-        app.post('/cadastro', async (req, res) => {
-            try {
-                res.send("ola");
-            } catch (err) {
-                console.log("Deu erro ao fazer a inserção", err);
-                res.status(500).send("Erro ao inserir dados.");
-            }
-        });
+    // Rota para login
+    app.post('/login', async (req, res) => {
+        try {
+            res.send("Olá");
+            console.log("Login bem sucedido");
+        } catch (err) {
+            console.log("Deu erro ao fazer login", err);
+            res.status(500).send("Erro ao fazer login.");
+        }
+    });
 
-     
-        app.post('/login', async (req, res) => {
-            try {
-                    res.send("ola")
-                    console.log("Login bem sucedido");
-            } catch (err) {
-                console.log("Deu erro ao fazer login", err);
-                res.status(500).send("Erro ao fazer login.");
-            }
-        });
-
-     
-        app.listen(port, () => {
-            console.log("O servidor está rodando na porta {port}");
-        });
-        
+    // Inicia o servidor
+    app.listen(port, () => {
+        console.log(`O servidor está rodando na porta ${port}`);
+    });
 }
 
 init();
-
